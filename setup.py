85bf116e4e3785d9a1c9af30cee845fd1c88279f
Ethan Chan <metakirby5@gmail.com>
2017-12-31 16:54:29 -0800

    version bump to 1.0.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ def package_files(directory):
 
 setup(
     name='webcon',
-    version='0.0.6',
+    version='1.0.0',
     description='Control your computer from a web interface',
     long_description=long_description,
     url='https://github.com/metakirby5/webcon',
